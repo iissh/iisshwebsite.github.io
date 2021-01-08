d4fd39e8bf90e2a2b68e1a8f0b08f5e185d17138
issh <46656303+iissh@users.noreply.github.com>
2021-01-08 10:13:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -41,12 +41,6 @@ $(document).ready(function(){
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Technology Enthusiast"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
 
     // owl carousel script
     $('.carousel').owlCarousel({
